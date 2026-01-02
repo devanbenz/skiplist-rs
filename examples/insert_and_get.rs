eccbce7055f98d9cca106225b1341831ae65b20e
devanbenz <devandbenz@gmail.com>
2026-01-02 17:12:42 -0600

    feat: Update example code

--- examples/insert_and_get.rs
+++ examples/insert_and_get.rs
@@ -5,12 +5,11 @@ fn main() {
     let sl = SkipList::<i32, i32, 6>::new();
     for i in 0..10_000 {
         println!("iteration {}", i);
-        let value = AtomicPtr::new(Box::into_raw(Box::new(i)));
-        sl.insert_inner(i, value);
+        sl.insert(i, i);
     }
 
     for i in 0..10_000 {
-        let v = sl.get_inner(&i);
+        let v = sl.get(&i);
         println!("{:?}", v);
     }
 }
