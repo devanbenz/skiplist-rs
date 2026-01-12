7b8a6dd5aa7ab3194fb2da58c5dcc9b4d85a9733
Devan <devandbenz@gmail.com>
2026-01-12 11:15:08 -0600

    feat: Working on crossbeam epoch skiplist

--- examples/scratch.rs
+++ examples/scratch.rs
@@ -1,5 +1,7 @@
 use crossbeam_epoch::{self as epoch, Atomic, Shared};
+use std::fmt::{Debug, Display};
 use std::iter::Skip;
+use std::ops::Deref;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
 
@@ -16,15 +18,17 @@ impl Min<i32> for i32 {
 struct Node<K, V, const N: usize> {
     key: K,
     value: Option<V>,
+    level: usize,
     forward: [Atomic<Node<K, V, N>>; N],
 }
 
 impl<K, V, const N: usize> Node<K, V, N> {
-    pub fn new(key: K, value: Option<V>) -> Self {
+    pub fn new(key: K, value: Option<V>, level: usize) -> Self {
         let forward = [const { Atomic::null() }; N];
         Self {
             key,
             value,
+            level,
             forward,
         }
     }
@@ -43,6 +47,32 @@ struct Skiplist<K, V, const N: usize> {
     head: Atomic<Node<K, V, N>>,
 }
 
+impl<K, V, const N: usize> Display for Skiplist<K, V, N>
+where
+    K: Display,
+    V: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let guard = &epoch::pin();
+        let head = self.head.load(Ordering::SeqCst, guard);
+        let head_node = unsafe { head.deref() };
+        for node in head_node.forward.iter() {
+            let node = node.load(Ordering::SeqCst, guard);
+            if !node.is_null() {
+                let mut val = String::from("null");
+                let node_key = unsafe { &node.deref().key };
+                let node_value = unsafe { &node.deref().value };
+                if let Some(v) = node_value {
+                    val = format!("{}", v);
+                }
+                write!(f, "key={}; value={}\n", node_key, val)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 impl<K, V, const N: usize> Skiplist<K, V, N>
 where
     K: Min<K> + std::cmp::PartialOrd,
@@ -51,7 +81,7 @@ where
         Self {
             cur_level: AtomicUsize::new(0),
             max_level: N,
-            head: Atomic::new(Node::new(K::min(), None)),
+            head: Atomic::new(Node::new(K::min(), None, N)),
         }
     }
 
@@ -62,7 +92,7 @@ where
 
         let mut update: [Shared<'_, Node<K, V, N>>; N] = [Shared::null(); N];
         let current_height = self.cur_level.load(Ordering::SeqCst);
-        for i in (0..=current_height).rev() {
+        for i in 0..self.max_level {
             loop {
                 let curr_node = unsafe { curr_ptr.deref() };
                 match curr_node.forward.get(i) {
@@ -85,12 +115,60 @@ where
             }
             update[i] = curr_ptr;
         }
+
+        let curr_node = unsafe { curr_ptr.deref() };
+        if let Some(next_ptr) = curr_node.forward.get(0) {
+            let mut next_node = next_ptr.load(Ordering::SeqCst, guard);
+            if !next_node.is_null() && unsafe { next_node.deref() }.key == key {
+                unsafe { next_node.deref_mut() }.value = Some(value);
+                return;
+            }
+        }
+
+        let lvl = Self::random_level(self.max_level);
+        let cur_level = self.cur_level.load(Ordering::Acquire);
+        if lvl > cur_level {
+            for i in cur_level + 1..=lvl {
+                update[i] = head_ptr;
+            }
+            self.cur_level.store(lvl, Ordering::Release);
+        }
+
+        let new_node_ptr = Atomic::new(Node::<K, V, N>::new(key, Some(value), lvl + 1));
+        let new_node = unsafe { new_node_ptr.load(Ordering::SeqCst, guard).deref() };
+        for i in (0..=lvl).rev() {
+            let prev_node = unsafe { update[i].deref() };
+            let next = prev_node.forward[i].load(Ordering::Acquire, guard);
+
+            new_node.forward[i].store(next, Ordering::Release);
+
+            prev_node.forward[i]
+                .compare_exchange(
+                    next,
+                    new_node_ptr.load(Ordering::Acquire, guard),
+                    Ordering::AcqRel,
+                    Ordering::Acquire,
+                    guard,
+                )
+                .expect("Failed to compare node");
+        }
+    }
+
+    fn random_level(max_level: usize) -> usize {
+        use rand::Rng;
+        let mut rng = rand::rng();
+        let mut level = 0;
+        while level < max_level - 1 && rng.random_bool(0.5) {
+            level += 1;
+        }
+        level
     }
 }
 
 fn main() {
     let skiplist: Skiplist<i32, i32, 3> = Skiplist::new();
-    skiplist.insert(10, 10);
-
-    println!("done!");
+    skiplist.insert(1, 2);
+    println!("before: {}", skiplist);
+    skiplist.insert(1, 4);
+    println!("after: {}", skiplist);
 }
